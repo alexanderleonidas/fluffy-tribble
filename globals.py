9d23b87d29657117a0d4347b17d12ed32f347c0f
timele23 <timos.eleftheriou@gmail.com>
2025-02-05 21:52:16 +0100

    added a function of happiness for each scheme,and the overall happiness in schemes.py
    function get_strategic_options function uses a mapping (method_mapping) so that it can dynamically call the correct voting and happiness methods for each scheme
    
    & compute_strategic_risk function iterates over all tactical options (from all schemes) and calculates a risk value based on the drop (if any) in overall and individual happiness. This value is then averaged and weighted by the fraction of voters with tactical options to produce an overall risk measure.

--- globals.py
+++ globals.py
@@ -6,4 +6,4 @@ NUM_CANDIDATES = 5
 
 # Number of voters using a strategy (<= NUM_VOTERS)
 # 1 is considered basic, more than 1 is advanced
-NUM_STRATEGIC_VOTERS = min(NUM_VOTERS, 1)
+NUM_STRATEGIC_VOTERS = min(NUM_VOTERS, 5)

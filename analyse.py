0ba6cb64204695f75b57018b54c819a5063cdbe9
diamenciarz <stasostaszewski@gmail.com>
2025-02-10 18:53:02 +0100

    updated analyse

--- analyse.py
+++ analyse.py
@@ -1,14 +1,20 @@
 from tva.situation import Situation
 from tva.schemes import Schemes
+from tva.enums import VotingScheme
+from tva.strategies import Strategies
 
-# Create the voting situation
-situation = Situation(num_voters=4, num_candidates=4, seed=42)
+strategies = Strategies()
+voting_scheme = VotingScheme.BORDA
+nr_repetitions = 1000
+num_voters = 4
 
-# Apply the voting scheme to the situation
-schemes = Schemes()
-print(situation)
-winner1 = schemes.anti_plurality_voting(situation.voters)
-winner2 = schemes.voting_for_two(situation.voters)
-winnner3 = schemes.borda_voting(situation.voters)
-print("Anti plurality:",winner1,", Two voting:", winner2, ", Borda:", winnner3)
-
+strategy_counter = 0
+for i in range(nr_repetitions):
+    situation = Situation(num_voters=num_voters, num_candidates=4)
+    # Check if at least one voter has a good strategy
+    for voter_index in range(num_voters):
+        if strategies.is_any_strategy_good(situation, voter_index, voting_scheme):
+            strategy_counter += 1
+            break
+        
+print(strategy_counter/nr_repetitions)

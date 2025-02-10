bc25b1ae3ab7c6db57d21cb103f42d5a85be3b44
diamenciarz <stasostaszewski@gmail.com>
2025-02-10 18:46:37 +0100

    preparing to pull from stan_strategy

--- tva/globals.py
+++
@@ -1,9 +0,0 @@
-# Number of voters
-NUM_VOTERS = 5
-
-# Number of candidates (maximum of 26)
-NUM_CANDIDATES = 5
-
-# Number of voters using a strategy (<= NUM_VOTERS)
-# 1 is considered basic, more than 1 is advanced
-NUM_STRATEGIC_VOTERS = min(NUM_VOTERS, 5)

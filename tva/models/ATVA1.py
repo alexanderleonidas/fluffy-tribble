2ff5f5ddfb201334939d313f192dcab91b7d16e6
shireen03 <91745024+shireen03@users.noreply.github.com>
2025-03-14 16:17:06 +0100

    Update ATVA1.py

--- tva/models/ATVA1.py
+++ tva/models/ATVA1.py
@@ -5,6 +5,7 @@ from collections import defaultdict
 from tva.models.BTVA import BTVA
 from tva.situation import Situation
 from tva.enums import HappinessFunc, VotingScheme, StrategyType
+import tqdm
 
 class ATVA1(BTVA):
     def __init__(self):
@@ -12,8 +13,6 @@ class ATVA1(BTVA):
 
     
     def merge_strategies(self, bury, bullets, comp):
-        # Merge bury, bullets and comp strategies
-
         merged = defaultdict(list)
         for strategy_dict in [bury, bullets, comp]:
             for voter_id, strategies in strategy_dict.items():
@@ -26,61 +25,57 @@ class ATVA1(BTVA):
                             existing_strategies.add(strategy_tuple)
         merged={k: v for k, v in merged.items() if v}
         voter_indexes = list(merged.keys())
-        #print(f"MERGED: {merged}")
-
-        print(f"VOTERS THAT HAVE STRATS: {voter_indexes}")
+  
         if len(voter_indexes)<2:
-            print("No collusions found due to limited strategic moves in the situation")
-            return False
-        return True, merged, voter_indexes 
+            return [False]
+        return [True, merged, voter_indexes]
 
 
-    def group_voters_by_preferences(self, preferences, strategies, stratVoters, election_ranking):
+    def group_voters_by_preferences(self, preferences, strategies, stratVoters, winner):
         voter_groups = defaultdict(list)
-        winner = election_ranking[0][0]
         # Do not consider collusion with voters who have their original preference as winner
 
-        dissatisfied_voters = {i: p for i, p in enumerate(preferences.voters) if p.preferences[0] != winner and i in strategies}
+        dissatisfied_voters = {p.voter_id: p.preferences for  p in preferences if p.preferences[0] != winner and p.voter_id in strategies}
         # Now group by common aim in first two preferences of all voters. 
         # Also if voter has unique preference (losing candidates), group them by themselves, and check their eligibility in joining another group with popular aim
         # it is eligible if the position of the other voter aim is lower than the current winner. 
         for voter, pref in dissatisfied_voters.items():
             added = False
             for aim in voter_groups.keys():
-                if (len(set(pref.preferences[:2]) & set(aim)) > 0):
+                if (len(set(pref[:2]) & set(aim)) > 0):
                     voter_groups[aim].append(voter)
                     added = True
-                    break
             if not added:
-                voter_groups[pref.preferences[0]].append(voter)
+                voter_groups[pref[0]].append(voter)
+                if pref[1]!=winner:
+                    voter_groups[pref[1]].append(voter) 
+
                 
-        print("")
-        print(f"Voter groups based on common top pref and unique prefs : {voter_groups}")
-        print("")
+        
         
         # find single voter groups 
         single_voter_groups = {key: voters for key, voters in voter_groups.items() if len(voters) == 1}
         # Get all aims
         aims = {key for key, voters in voter_groups.items() if len(voters) > 1}
-        print(f"aims combined: {aims}")
-        print("")
+
         # These single groups can represent compromoising since they are voters with no common top preference.
         # Try find a new aim for these single groups (expecting compromise to another aim).
         # Check for each aim among other voter groups if the aim (the candidate) position is before the winner position in the og preference of the single voter.
         #If it is, then we can append this voter to the "aim" (compromising to popular strategic aim)
         for key, voters in single_voter_groups.items():
+            delete_key = False  # Initialize here for each group
             voter = voters[0]
-            pref = preferences.voters[voter].preferences
+            pref = preferences[voter].preferences
             for aim in aims:
                 if pref.index(aim) < pref.index(winner):  
-                    voter_groups[aim].append(voter)
-                    del voter_groups[key]
-                    
+                    voter_groups[aim].append(voter)            
+            del voter_groups[key]
+                
         return voter_groups
       
 
     # Get combos of voter collusions based on group (can be up to size 3 (adjustable))
-    def form_voter_pairs(self, voter_groups, max_size=3):
+    def form_voter_pairs(self, voter_groups, max_size):
         coalitions = []
         for key, voters in voter_groups.items():
             if len(voters) < 2:
@@ -91,11 +86,10 @@ class ATVA1(BTVA):
         return coalitions
 
 
-    def collude(self, coalitions, original_election_outcome, strategies, situation:Situation, voting_scheme:VotingScheme, happiness_func:HappinessFunc):
-        winner = original_election_outcome[0][0]
+    def collude(self, coalitions, rank, original_total_happiness, strategies, situation:Situation, voting_scheme:VotingScheme, happiness_func:HappinessFunc):
+       
         #Sort the coalitions by the number of voters. e.g. ('A', (1,3,5)) has 3 voters with aim to have A win. Sorts it by most voters with aim.
         coalitions = sorted(coalitions, key=lambda x: -len(x[1]))
-        print(f"Sorted coalitions: {coalitions}")
 
         #best_coalition = None
         #max_happiness_gain = float('-inf')
@@ -104,7 +98,7 @@ class ATVA1(BTVA):
 
         for aim, voters in coalitions:
             temp_situation: Situation = deepcopy(situation)
-            best_strategies = self.select_best_strategy(voters, strategies, winner, aim)
+            best_strategies = self.select_best_strategy(voters, strategies, rank, aim)
     
             # Filter out voters who don't have a strategy towards aim
             voters = tuple(voter for voter in voters if voter in best_strategies)
@@ -128,7 +122,7 @@ class ATVA1(BTVA):
                 
                 # Get happiness gain sum among all colluding voters. We choose the biggest one.
                 happiness_gain = sum(temp_individual_h.get(voter, 0) - strategies[voter][0]['original_individual_happiness'] for voter in voters)
-                
+                total_happiness_gain=temp_total_h - original_total_happiness
                 happiness_valid = any(
                     temp_individual_h.get(voter, 0) > strategies[voter][0]['original_individual_happiness'] for voter in voters) and all(temp_individual_h.get(voter, 0) >= strategies[voter][0]['original_individual_happiness'] for voter in voters)
                 
@@ -155,83 +149,84 @@ class ATVA1(BTVA):
                 if happiness_valid:
                     coalition_info = {
                         'coalition': voters,
-                        'original_winner': winner,
+                        'original_winner': rank[0][0],
                         'simulated_winner': new_winner,
-                        'total_happiness_gain': happiness_gain,
+                        'colluder_total_happiness_gain': happiness_gain,
                         'new_total_happiness': temp_total_h,
+                        'total_happiness_gain': total_happiness_gain,
                         'colluders_original_happiness': colluders_original_happiness,
                         'colluders_strategic_happiness': colluders_strategy_happiness,
                         'colluders_happiness_gain': individual_happiness_gain,
                         'strategic_collusion_action' : strategic_actions
                     }
-                    print("")
-                    print("COALITION FOUND:")
-                    print(coalition_info)
-                    return coalition_info
-                #coalition_results.append(coalition_info)
-
-                # Track best coalition
-                #if happiness_gain > max_happiness_gain:
-                    #max_happiness_gain = happiness_gain
-                    #best_coalition = coalition_info
                 
-                #return coalition_info
-          
+                    return coalition_info
 
-        # Print the best coalition
-        #if best_coalition:
-            #print("")
-            #print("Best Coalition:")
-            #print(best_coalition)
-        print("No collusions found")
         return False
 
 
-    def select_best_strategy(self, voters, strategies, original_winner, aim):
+    def select_best_strategy(self, voters, strategies, rank, aim):
         selected_strategies = {}
+        winner=rank[0][0]
+        
         # Check for each voter in group based on aim
         for voter in voters:
             if voter not in strategies:
                 continue
             for strategy in strategies[voter]:
                 #AIM which is one of their top 2 prefs should be the first in their strategy. the og winner should be in bottom half.
-                if strategy['strategy'][0] == aim and original_winner in strategy['strategy'][len(strategy['strategy']) // 2:]:
+                if strategy['strategy'][0] == aim and winner in strategy['strategy'][len(strategy['strategy']) // 2:]:
                     selected_strategies[voter] = strategy
                     break
-        #print("")
-        #print(f"Selected strategies: {selected_strategies}")
+
         return selected_strategies
 
-atva = ATVA1()
-#situation = Situation(30, 8, seed=220)
-situation = Situation(15, 8, seed=120)
+    def analyse_multiple_ATVA(self, situations,num_repititions, voting_scheme, happiness_func, max_collusion, verbose=False):
+  
+        counter = 0
+        happinessGain = 0
 
-preferences = situation
-happiness_func = HappinessFunc.WEIGHTED_POSITIONAL
-voting_scheme = VotingScheme.VOTE_FOR_TWO
-strategy_types = [StrategyType.BULLET, StrategyType.BURYING, StrategyType.COMPROMISING]
+        for situation in situations:
 
-bullets = atva.analyse_single(situation, happiness_func, voting_scheme, StrategyType.BULLET, True)
-bury = atva.analyse_single(situation, happiness_func, voting_scheme, StrategyType.BURYING, False)
-comp = atva.analyse_single(situation, happiness_func, voting_scheme, StrategyType.COMPROMISING, False)
+            bullet=self.analyse_single(situation,happiness_func,voting_scheme,StrategyType.BULLET,False)
+            bury=self.analyse_single(situation,happiness_func,voting_scheme,StrategyType.BURYING,False)
+            comp=self.analyse_single(situation,happiness_func,voting_scheme,StrategyType.COMPROMISING,False)
+            
+            result = self.merge_strategies(bury=bury,bullets=bullet,comp=comp)
+            if result[0]:  # if there is more than one voter with strategic moves. Otherwise no collusion possible
+                merged_strats, stratVoters = result[1], result[2]  
+                total_h, individual_h, original_winner = situation.calculate_happiness(situation.voters, happiness_func, voting_scheme, return_winner=True) # type: ignore
+                original_rank = self.schemes.apply_voting_scheme(voting_scheme, situation.voters, True, True)
 
-#print(f"BULLET STRATS: {bullets}")
-#print(f"BURY STRATS: {bury}")
-#print(f"COMP STRATS: {comp}")
+                groups = self.group_voters_by_preferences(situation.voters, merged_strats, stratVoters, original_winner)
+                coalitions = self.form_voter_pairs(groups, max_collusion)
 
-print("-------------------------------------")
-print("ATVA1 collusion information:")
+                collusion_stats=self.collude(coalitions, original_rank, total_h, merged_strats, situation, voting_scheme, happiness_func)
+            
+                if collusion_stats and isinstance(collusion_stats, dict):
+                    counter+=1
+                    if collusion_stats['new_total_happiness'] > total_h:
+                        happinessGain += 1
+                        
+                        
+        atva1_risk = (counter / (num_repititions)) * 100
+        happiness_improvement= (happinessGain/num_repititions)*100
 
-result = atva.merge_strategies(bullets, bury, comp)
+        return {
+            'atva1_risk': atva1_risk,
+            'happiness_improvement': happiness_improvement
+        }
 
-if result:  # if there is more than one voter with strategic moves. Otherwise no collusion possible
-    merged_strats, stratVoters = result[1], result[2]  
-    
-    original_election_outcome = atva.schemes.apply_voting_scheme(voting_scheme, preferences.voters, True, True)
-    print("")
-    print(f"original election outcome : {original_election_outcome}")
+    def analyse_multiple_for_comparison(self, situations, voting_scheme, happiness_func, verbose=False):
+        btva_risks = {StrategyType.BULLET:0.0, 
+                      StrategyType.BURYING:0.0,
+                      StrategyType.COMPROMISING: 0.0}
+        
+        for k, _ in btva_risks.items():
+            risk = self.analyse_multiple(situations, voting_scheme, happiness_func, k, verbose)
+            btva_risks[k] = risk
+        
+        return btva_risks
+           
 
-    groups = atva.group_voters_by_preferences(preferences, merged_strats, stratVoters, original_election_outcome)
-    coalitions = atva.form_voter_pairs(groups, 5)
-
-    atva.collude(coalitions, original_election_outcome, merged_strats, situation, voting_scheme, happiness_func)
+           
